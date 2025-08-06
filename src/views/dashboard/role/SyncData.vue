9661439cbd097be6a4f94aad8de8a085aa530106
iRuxu <rx6@qq.com>
2025-08-06 11:41:11 +0800

    perf:styles

--- src/views/dashboard/role/SyncData.vue
+++ src/views/dashboard/role/SyncData.vue
@@ -3,7 +3,7 @@
         <h2 class="u-title">
             <i class="el-icon-connection"></i> 同步数据
             <el-button slot="reference" class="u-back" size="mini" icon="el-icon-arrow-left" @click="goBack"
-                >返回列表</el-button
+                >返回</el-button
             >
         </h2>
         <div class="m-tutorial" v-html="notice"></div>
@@ -20,8 +20,8 @@ export default {
         }
     },
     methods: {
-        goBack() {
-            this.$router.push("/role/list");
+        goBack: function () {
+            this.$router.push("/role");
         },
         loadBreadCrumb() {
             getBreadcrumb("user-sync-game-data").then((res) => {
@@ -38,18 +38,18 @@ export default {
 
 <style lang="less">
 .v-sync-data {
-    padding: 20px;
     .u-title {
         margin-bottom: 20px;
     }
     .m-tutorial {
-        background-color: #f5f7fa;
-        border:1px solid #eee;
-        padding:30px;
-        line-height:1.8;
-        p{margin:0 0 5px 0;}
+        // background-color: #f5f7fa;
+        // border:1px solid #eee;
+        // padding:30px;
+        // line-height:1.8;
+        // p{margin:0 0 5px 0;}
         max-width:1080px;
         .auto(x);
+        .x;
     }
 }
 </style>
