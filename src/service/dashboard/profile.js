5cfa2a8f570046ed11298ac1eeb267b4c805ef81
iRuxu <rx6@qq.com>
2026-07-10 03:54:49 +0800

    ci:merge

--- src/service/dashboard/profile.js
+++ src/service/dashboard/profile.js
@@ -9,9 +9,6 @@ import { encryptPassword } from "@/utils/pwd_encrypt";
 function getProfile() {
     return $cms().get("/api/cms/user/my/profile");
 }
-function getPasswordStatus() {
-    return $cms().get("/api/cms/user/my/password/status");
-}
 function updateProfile(data) {
     return $cms().put("/api/cms/user/my/profile", data);
 }
@@ -145,7 +142,6 @@ function checkPhone(params) {
 export {
     updateProfile,
     getProfile,
-    getPasswordStatus,
     updateAvatar,
     updatePassword,
     getPasswordStatus,
